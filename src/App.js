5bf1b4604ef582d001d2f873b60378770a5dd630
amirkumar24102000 <gulshankumar24102000@gmail.com>
2022-07-15 09:06:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next'
+
+
+// Contains the value and text for the options
+const languages = [
+	{ value: '', text: "Options" },
+	{ value: 'en', text: "English" },
+	{ value: 'hi', text: "Hindi" },
+	{ value: 'ur', text: "Urdu" }
+]
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	// It is a hook imported from 'react-i18next'
+	const { t } = useTranslation();
+
+	const [lang, setLang] = useState('');
+
+	// This function put query that helps to
+	// change the language
+	const handleChange = e => {
+    // e.prevent.Default()
+		setLang(e.target.value);
+		let loc = "http://localhost:3000/";
+		window.location.replace(loc + "?lng=" + e.target.value);
+	}
+
+	return (
+		<div className="App">
+
+			
+			<h1>{t('Home')}</h1>
+			<label>{t('choose language value')}</label>
+
+			<select value={lang} onChange={handleChange}>
+				{languages.map(item => {
+					return (<option key={item.value}
+					value={item.value}>{item.text}</option>);
+				})}
+			</select>
+		</div>
+	);
 }
 
 export default App;
